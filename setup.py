d03261cc942dfcf187c2e246247d751730f0dc68
Prem Adithya <adithyaxx@users.noreply.github.com>
2020-07-28 11:40:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with io.open('README.md', 'rt', encoding="utf-8") as readme_file:
 
 
 setup(
-    name='jsonobject',
+    name='gj-jsonobject',
     version='0.9.9',
     author='Danny Roberts',
     author_email='droberts@dimagi.com',
